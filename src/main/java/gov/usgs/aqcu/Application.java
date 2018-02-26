5e38ca40283aaa66474589bab70fc5ebb858b423
Steinich, Dave R. <drsteini@usgs.gov>
2018-02-26 09:30:17 -0600

    ugly initial commit

---
+++ src/main/java/gov/usgs/aqcu/Application.java
@@ -0,0 +1,29 @@
+package gov.usgs.aqcu;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+//@EnableHystrixDashboard
+//@EnableZuulProxy
+//@EnableFeignClients
+//@EnableHystrix
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/gov/usgs/aqcu/config/WebMvcConfig.java
@@ -0,0 +1,37 @@
+package gov.usgs.aqcu.config;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.WebJarsResourceResolver;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**")
+				.addResourceLocations("/resources/", "/webjars/")
+				.setCacheControl(
+						CacheControl.maxAge(30L, TimeUnit.DAYS).cachePublic())
+				.resourceChain(true)
+				.addResolver(new WebJarsResourceResolver());
+	}
+
+}
